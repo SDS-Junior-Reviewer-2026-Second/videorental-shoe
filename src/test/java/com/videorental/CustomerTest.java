ae67ccdfc948ba0941279830f28b6eb376ae3a74
박시우 <pswudo@gmail.com>
2026-09-16 16:16:57 +0900

    test: set up customer characterization tests

--- src/test/java/com/videorental/CustomerTest.java
+++ src/test/java/com/videorental/CustomerTest.java
@@ -1,5 +1,24 @@
 package com.videorental;
 
-public class CustomerTest {
+import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CustomerTest {
+    @Test
+    public void returnNewCustomer() {
+        Customer customer = new Customer("Name_Not_Important");
+        assertThat(customer).isNotNull();
+    }
+    
+    @Test
+    public void statementForNoRental() {
+        Customer customer = new Customer("NAME_NOT_IMPORTANT");
+
+        assertThat(customer.statement()).isEqualTo(
+                "Rental Record for NAME_NOT_IMPORTANT\n"
+                        + "Amount owed is 0.0\n"
+                        + "You earned 0 frequent renter pointers"
+        );
+    }
 }
